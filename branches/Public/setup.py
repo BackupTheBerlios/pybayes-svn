ad59f4e025d8a4ef792a59ad8b485058225c1e03
kgaitanis <kgaitanis@92d5be9a-8c06-0410-b7f8-ccae94bcdc48>
2006-04-03 14:08:28 +0000

    Created a new branch called 'Public'
    This branch will contain the files that will be publicly available on the web site
    + a directory inside this branch called 'dist' with the source files (.tar.gz) and a windows installer file (.exe)
    
    The point of this branch is to create a clean branch with no unnecessary code.
    
    How am I going to maintain it clear???

---
+++ branches/Public/setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+import sys
+
+# patch distutils if it can't cope with the "classifiers" or
+# "download_url" keywords
+if sys.version < '2.2.3':
+    from distutils.dist import DistributionMetadata
+    DistributionMetadata.classifiers = None
+    DistributionMetadata.download_url = None
+
+##      py_modules = ['bayesnet','graph','inference','distributions','potentials',
+##                  'table','delegate'],
+
+setup(  name='OpenBayes',
+        version='0.1.0',
+        description='An OpenSource Python implementation of bayesian networks inspired by BNT.',
+        author = 'Kosta Gaitanis, Elliot Cohen',
+	author_email = 'gaitanis@tele.ucl.ac.be, elliot.cohen@gmail.com',
+	url='http://www.openbayes.org',
+        packages = ['OpenBayes'],
+        package_dir = {'OpenBayes':'./'},
+	license = 'modified Python',
+	classifiers=[
+          'Development Status :: First Public Release',
+          'Environment :: Console',
+          'Intended Audience :: Science/Research',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Python Software Foundation License',
+	  'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Bayesian Networks',
+          'Topic :: Probabilistic Inference',
+          'Topic :: Learning Graphical models',
+          ],
+)
